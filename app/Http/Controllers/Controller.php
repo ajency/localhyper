a5eecc4aacb47a1cc50c05c98635a3a801452329
Nutan Kamat <nutan@ajency.in>
2015-06-26 13:46:18 +0530

    add parse account specific keys and initialize parse

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+use Parse\ParseClient;
+
+
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+    	ParseClient::initialize(config('constants.parse_sdk.app_id'), config('constants.parse_sdk.rest_api_key'),config('constants.parse_sdk.master_key'));
+    }
 }
---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'parse_sdk' => [
+        'app_id' => 'XGPT3nZCUbaP8FFHneekeGpRaTXju7mfsnex7j2T',
+        'rest_api_key' => 'Vf1JfTHGicmQxP2G0Lw4Cd1lsRkVKs6S3DYd979p',
+        'master_key' => 'pOVGBIcjIJters9P3ELEm1hsJOnBnwfrh0WQ6x80'
+    ]
+];
