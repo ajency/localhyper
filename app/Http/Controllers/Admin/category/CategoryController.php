f89939c164ecb8f98bfed31408dc49b44241c7f0
Nutan Kamat <nutan@ajency.in>
2015-06-26 18:20:05 +0530

    add categories

--- app/Http/Controllers/Admin/category/CategoryController.php
+++ app/Http/Controllers/Admin/category/CategoryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Parse\ParseObject;
 
 class CategoryController extends Controller
 {
@@ -41,9 +42,26 @@ class CategoryController extends Controller
      * @return Response
      */
     // for post method and path /category
-    public function store()
+    public function store(Request $request)
     {
-        //
+       
+        $category_name = $request->input('category_name');
+        $parent_category = $request->input('parent_category');
+        $sort_id = $request->input('sort_id');
+
+        $categoryData = $request->all();
+        $catData = array(
+            'name' => $categoryData['category_name'], 
+            'description' => $categoryData['description'], 
+            'sort_order' => (int)$categoryData['sort_id'], 
+            'image' => array('src' => $categoryData['image_url']), 
+            );
+
+        $category = CategoryController::createParseCategory($catData);
+
+        // return redirect("/admin/category/".$category);
+        return redirect("/admin/category/create");
+
     }
 
     /**
@@ -93,5 +111,30 @@ class CategoryController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public static function createParseCategory($categoryData){
+       
+        $category = new ParseObject("Category");
+        $category->set("name", $categoryData['name']);
+        $category->set("description", $categoryData['description']);
+        // $category->set("parent_category", $categoryData['parent_category']);
+        $category->set("sort_order", $categoryData['sort_order']);
+        $category->setArray("image", $categoryData['image']);
+        
+        try {
+          $category->save();
+          // dd($category->getObjectId());
+          // echo 'New object created with objectId: ' . $category->getObjectId();
+          return $category->getObjectId();
+        } 
+        catch (ParseException $ex) {  
+          // Execute any logic that should take place if the save fails.
+          // error is a ParseException object with an error code and message.
+              echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+              return $ex->getMessage();
+
+        }
+
     }
 }
--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -8,67 +8,56 @@
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
-<!-- BEGIN PAGE TITLE -->
-<div class="page-title">	
-    <h2><span class="semi-bold">Add</span> Category</h2>
-</div>
-<!-- END PAGE TITLE -->
-<!-- BEGIN PlACE PAGE CONTENT HERE -->
-<div class="row">
-    <div class="col-md-12">
-        <div class="grid simple">
-            <div class="grid-body no-border"> 
-                <br>
-                <form id="add_project" method="POST" action="admin/category" data-parsley-validate>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label class="form-label">Name<span class="text-primary">*</span></label>
-                                <input type="text" name="name" class="form-control m-b-5" placeholder="Enter Category Name" data-parsley-required>
-                                                           </div>
-                        </div>
-                       
-                        </div>
-                         <div class="row">
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label class="form-label">Parent<span class="text-primary">*</span></label>
-<!--                                 <select name="user_role" class="select2 form-control m-b-5" data-parsley-required>
-                                    <option value="">Select Parent Category</option>
-                                    
-                                    
-                                   
-                                </select> -->
-                              <input type="text" name="name" class="form-control m-b-5" placeholder="Enter Parent Category" data-parsley-required>
-                            </div>
-                        </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label class="form-label">Sort ID<span class="text-primary">*</span></label>
-                                    <input type="text" name="name" class="form-control m-b-5" placeholder="Sort Id" data-parsley-required>
-                                    
-                                </div>
-                            </div>
-                       </div>
-                              
 
-                    <div class="form-actions "> 
-                        <div class="pull-right">
-                            <input type="hidden" id="addanother" name="addanother" value="">
-                            <input type="hidden" value="{{ csrf_token()}}" name="_token"/>
-                            <button type="submit" class="btn btn-primary btn-cons"><i class="fa fa-plus-circle"></i> Create</button>
-                            <button type="button" onclick="saveAndAddAnother();" class="btn btn-default btn-cons">Save And Create Another</button>
-                            <button type="reset" class="hidden" />
-                            <a href="{{ url('admin/user') }}"><button type="button" class="btn btn-default btn-cons"><i class="fa fa-ban"></i> Cancel</button></a>
 
-                        </div>
+				<div id="add-folder-div">
+					<h5>Add a Category</h5>
+					<hr>
+					<div class="row">
+						<div class="col-sm-4">
+							<form id="add_project" method="POST" action="{{ url('admin/category') }}" data-parsley-validate>
 
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+								<div class="form-group fly-group">
+							        <label class="fly-label classic">Category Name</label>           
+							        <input type="text" class="form-control input-sm" id="" name="category_name" placeholder="Enter Category Name" data-parsley-required>
+								</div>
+
+							    <div class="form-group fly-group">
+						            <label class="fly-label classic">Description </label>
+						            <textarea class="form-control" rows="3" name="description" placeholder="Type your Description here"></textarea>
+						        </div>
+
+						        <div class="form-group fly-group">
+						        	<label class="fly-label classic">Parent Category</label>
+
+						        	<select class="form-control js-select-parent-folder-tree" name="parent_category" required=""data-parsley-required>
+						        		<option value="">Select Parent</option>
+						        		<option value="0">None</option>
+						        	<!-- <option value="209">Root/Firms/Ascot Wealth Management system</option>
+						        	<option value="199">Root/Firms/Clients</option> -->
+						        	</select> 
+						        </div>
+				                
+							    <div class="form-group fly-group">
+						            <label class="fly-label classic">Sort ID</label>
+						        	<input type="text" class="form-control input-sm" id="" name="sort_id" placeholder="Enter sort id" data-parsley-required>
+						                     
+						        </div>	
+							    <div class="form-group fly-group">
+						            <label class="fly-label classic">Image Url</label>
+						        	<input type="text" class="form-control input-sm" id="" name="image_url" placeholder="Enter image url" data-parsley-required>
+						                     
+						        </div>							        
+						        <button type="submit" class="btn btn-primary btn-sm parsley-err-btn parsley-err-btn">Add New Category</button>
+						        <a href="{{ url('admin/category') }}" class="btn btn-link">Cancel</a>
+						        <input type="hidden" value="{{ csrf_token()}}" name="_token"/>
+
+							</form>
+						</div>
+					</div>
+
+				</div>
+					
 				</div>
 			</div>
 		</div>
