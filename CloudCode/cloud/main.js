1be96c366c88c39a258cbc23a0e20af9cf860fc9
Nutan Kamat <nutan@ajency.in>
2015-07-02 10:56:58 +0530

    products import with product data in request to the parse job

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -81,67 +81,52 @@
   });
 
   Parse.Cloud.job('productImport', function(request, response) {
-    var BulkImport, query, queryFindPromise;
-    BulkImport = Parse.Object.extend('bulkImport');
-    query = new Parse.Query(BulkImport);
-    query.include("import_based_id");
-    queryFindPromise = query.find();
-    queryFindPromise.done((function(_this) {
-      return function(results) {
-        var ProductItem, productSavedArr;
-        ProductItem = Parse.Object.extend('ProductItem');
-        productSavedArr = [];
-        _.each(results, function(result) {
-          var products;
-          products = result.get("json");
-          return _.each(products, function(product) {
-            var attributeValueArr, attributes, brandObj, categoryObj, productItem;
-            productItem = new ProductItem();
-            productItem.set("name", product.name);
-            productItem.set("images", product.images);
-            productItem.set("model_number", product.model_number);
-            productItem.set("mrp", parseInt(product.mrp));
-            productItem.set("popularity", product.popularity);
-            productItem.set("group", product.group);
-            categoryObj = {
-              "__type": "Pointer",
-              "className": "Category",
-              "objectId": product.category
-            };
-            productItem.set("category", categoryObj);
-            brandObj = {
-              "__type": "Pointer",
-              "className": "Brand",
-              "objectId": product.brand
-            };
-            productItem.set("brand", brandObj);
-            attributeValueArr = [];
-            attributes = product.attrs;
-            _.each(attributes, function(attributeId) {
-              var attribObj;
-              attribObj = {
-                "__type": "Pointer",
-                "className": "AttributeValues",
-                "objectId": attributeId
-              };
-              return attributeValueArr.push(attribObj);
-            });
-            productItem.set("attrs", attributeValueArr);
-            return productSavedArr.push(productItem);
-          });
-        });
-        return Parse.Object.saveAll(productSavedArr, {
-          success: function(objs) {
-            response.success("Successfully added the products");
-          },
-          error: function(error) {
-            return response.error("Failed to add products due to - " + error.message);
-          }
-        });
+    var ProductItem, productSavedArr, products;
+    ProductItem = Parse.Object.extend('ProductItem');
+    productSavedArr = [];
+    products = request.params.products;
+    _.each(products, function(product) {
+      var attributeValueArr, attributes, brandObj, categoryObj, productItem;
+      productItem = new ProductItem();
+      productItem.set("name", product.name);
+      productItem.set("images", product.images);
+      productItem.set("model_number", product.model_number);
+      productItem.set("mrp", parseInt(product.mrp));
+      productItem.set("popularity", product.popularity);
+      productItem.set("group", product.group);
+      categoryObj = {
+        "__type": "Pointer",
+        "className": "Category",
+        "objectId": product.category
       };
-    })(this));
-    return queryFindPromise.fail(function(error) {
-      return response.error("Error in products upload - " + error.message);
+      productItem.set("category", categoryObj);
+      brandObj = {
+        "__type": "Pointer",
+        "className": "Brand",
+        "objectId": product.brand
+      };
+      productItem.set("brand", brandObj);
+      attributeValueArr = [];
+      attributes = product.attrs;
+      _.each(attributes, function(attributeId) {
+        var attribObj;
+        attribObj = {
+          "__type": "Pointer",
+          "className": "AttributeValues",
+          "objectId": attributeId
+        };
+        return attributeValueArr.push(attribObj);
+      });
+      productItem.set("attrs", attributeValueArr);
+      return productSavedArr.push(productItem);
+    });
+    return Parse.Object.saveAll(productSavedArr, {
+      success: function(objs) {
+        response.success("Successfully added the products");
+      },
+      error: function(error) {
+        return response.error("Failed to add products due to - " + error.message);
+      }
     });
   });
 
--- CloudCode/source/entities/product.coffee
+++ CloudCode/source/entities/product.coffee
@@ -67,73 +67,62 @@
 
 Parse.Cloud.job 'productImport', (request, response) ->
 
-	# get product json to be imported
-	BulkImport = Parse.Object.extend('bulkImport')
+	ProductItem = Parse.Object.extend('ProductItem')
 
-	query = new Parse.Query(BulkImport)
-	query.include("import_based_id")
-	queryFindPromise = query.find()
-	queryFindPromise.done (results) =>
-		ProductItem = Parse.Object.extend('ProductItem')
+	productSavedArr = []
 
-		productSavedArr = []
+	products =  request.params.products
 
-		_.each results, (result) ->
-			
-			products =  result.get("json")
+	_.each products, (product) ->
+		productItem = new ProductItem()
+		productItem.set "name", product.name
+		productItem.set "images", product.images
+		productItem.set "model_number", product.model_number
+		productItem.set "mrp", parseInt product.mrp
+		productItem.set "popularity", product.popularity
+		productItem.set "group", product.group
 
-			_.each products, (product) ->
-				productItem = new ProductItem()
-				productItem.set "name", product.name
-				productItem.set "images", product.images
-				productItem.set "model_number", product.model_number
-				productItem.set "mrp", parseInt product.mrp
-				productItem.set "popularity", product.popularity
-				productItem.set "group", product.group
+		# set product category
+		categoryObj = 
+			"__type" : "Pointer",
+			"className":"Category",
+			"objectId":product.category
 
-				# set product category
-				categoryObj = 
-					"__type" : "Pointer",
-					"className":"Category",
-					"objectId":product.category
-
-				productItem.set "category", categoryObj	
-				
-				# set brand
-				brandObj =
-					"__type" : "Pointer",
-					"className":"Brand",
-					"objectId":product.brand					
-
-				productItem.set "brand", brandObj	
-				
-				attributeValueArr = []
-				attributes = product.attrs
-
-				_.each attributes, (attributeId) ->
-					attribObj = 
-						"__type" : "Pointer",
-						"className":"AttributeValues",
-						"objectId":attributeId
-
-					attributeValueArr.push(attribObj)
-
-				productItem.set "attrs", attributeValueArr						
-
-
-				productSavedArr.push(productItem)
-			
-	
-		# save all the newly created objects
-		Parse.Object.saveAll productSavedArr,
-		  success: (objs) ->
-		    response.success "Successfully added the products"
-		    return
-		  error: (error) ->
-		    response.error "Failed to add products due to - #{error.message}"
+		productItem.set "category", categoryObj	
 		
-	queryFindPromise.fail (error) ->
-		response.error "Error in products upload - #{error.message}"
+		# set brand
+		brandObj =
+			"__type" : "Pointer",
+			"className":"Brand",
+			"objectId":product.brand					
+
+		productItem.set "brand", brandObj	
+		
+		attributeValueArr = []
+		attributes = product.attrs
+
+		_.each attributes, (attributeId) ->
+			attribObj = 
+				"__type" : "Pointer",
+				"className":"AttributeValues",
+				"objectId":attributeId
+
+			attributeValueArr.push(attribObj)
+
+		productItem.set "attrs", attributeValueArr						
+
+
+		productSavedArr.push(productItem)
+		
+
+	# save all the newly created objects
+	Parse.Object.saveAll productSavedArr,
+	  success: (objs) ->
+	    response.success "Successfully added the products"
+	    return
+	  error: (error) ->
+	    response.error "Failed to add products due to - #{error.message}"
+
 	
 
 	
