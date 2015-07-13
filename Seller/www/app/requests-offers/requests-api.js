6017225460322ca8b079bcd276689f03be4427c6
Deepak Prabhudesai <deepak@ajency.in>
2015-07-13 15:53:12 +0530

    no message

---
+++ Seller/www/app/requests-offers/requests-api.js
@@ -0,0 +1,25 @@
+angular.module('LocalHyper.requestsOffers').factory('RequestsAPI', [
+  '$q', '$http', 'User', function($q, $http, User) {
+    var RequestsAPI;
+    RequestsAPI = {};
+    RequestsAPI.getAll = function() {
+      var defer, params, user;
+      defer = $q.defer();
+      user = User.getCurrent();
+      params = {
+        "sellerId": user.id,
+        "city": user.get('city'),
+        "area": user.get('area'),
+        "sellerLocation": "default",
+        "sellerRadius": "default"
+      };
+      $http.post('functions/getNewRequests', params).then(function(data) {
+        return defer.resolve(data.data.result);
+      }, function(error) {
+        return defer.reject(error);
+      });
+      return defer.promise;
+    };
+    return RequestsAPI;
+  }
+]);
--- Seller/www/source/requests-offers/new-requests.coffee
+++ Seller/www/source/requests-offers/new-requests.coffee
@@ -1,9 +1,37 @@
 angular.module 'LocalHyper.requestsOffers'
 
 
-.controller 'NewRequestCtrl', ['$scope', 'App', ($scope, App)->
+.controller 'NewRequestCtrl', ['$scope', 'App', 'RequestsAPI', ($scope, App, RequestsAPI)->
 
+	$scope.view = 
+		display: 'loader'
+		errorType: ''
+		requests: []
 
+		init : ->
+			@getRequests()
+
+		getRequests : ->
+			RequestsAPI.getAll()
+			.then (data)=>
+				console.log data
+				@onSuccess data
+			, (error)=>
+				@onError error
+
+		onSuccess : (data)->
+			@display = 'noError'
+			@requests = data.requests
+		
+		onError: (type)->
+			@display = 'error'
+			@errorType = type
+
+		onTapToRetry : ->
+			@display = 'loader'
+			@getRequests()
+
+	
 	$scope.$on '$ionicView.afterEnter', ->
 		App.hideSplashScreen()
 ]
