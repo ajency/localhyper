b25f9027d12c03dffe3b3265c83842a42cc58671
Nutan Kamat <nutan@ajency.in>
2015-07-22 12:56:07 +0530

    gulp coffee setup

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+(function() {
+  var getSellers;
+
+  getSellers = function() {
+    var sellers;
+    sellers = [
+      {
+        'objectId': 'waX1yFgxqy',
+        'name': 'energy saving ratings',
+        'group': 'general',
+        'unit': 'stars',
+        'is_primary': 'no'
+      }, {
+        'objectId': 'TFXHBzAPOO',
+        'name': 'ac type',
+        'group': 'general',
+        'unit': null,
+        'is_primary': 'no'
+      }
+    ];
+    return sellers;
+  };
+
+}).call(this);
+
+//# sourceMappingURL=app.js.map
---
+++ resources/assets/coffee/backend/users.coffee
@@ -0,0 +1,19 @@
+getSellers =() ->
+	sellers = [
+	  {
+	    'objectId': 'waX1yFgxqy'
+	    'name': 'energy saving ratings'
+	    'group': 'general'
+	    'unit': 'stars'
+	    'is_primary': 'no'
+	  }
+	  {
+	    'objectId': 'TFXHBzAPOO'
+	    'name': 'ac type'
+	    'group': 'general'
+	    'unit': null
+	    'is_primary': 'no'
+	  }
+	]	
+  	
+	return sellers
