aeef551b9c59d7ca2db8af690911bc566e74baba
Robiul Hoque <robiul@ajency.in>
2015-08-07 16:06:39 +0530

    Image process functionality: fetch the images from remote url, process into various sizes and put it back to amazon bucket.

--- config/app.php
+++ config/app.php
@@ -145,6 +145,9 @@ return [
         App\Providers\RouteServiceProvider::class,
         Bestmomo\Scafold\ScafoldServiceProvider::class,
 
+        'Aws\Laravel\AwsServiceProvider',
+        'Intervention\Image\ImageServiceProvider',
+
     ],
 
     /*
@@ -192,6 +195,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'AWS'       => Aws\Laravel\AwsFacade\AWS::class,
+        'AWS'       => 'Aws\Laravel\AwsFacade',
+        'Image'     => 'Intervention\Image\Facades\Image'
 
     ],
 
