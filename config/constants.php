d9332787744feee5f16c6a53826520371f1e08ea
Prajay <prajay@ajency.in>
2015-08-10 17:41:15 +0530

    added page limit

--- config/constants.php
+++ config/constants.php
@@ -1,17 +1,19 @@
 <?php
 
 return [
-    'parse_sdk' => [
+    /*'parse_sdk' => [
         'app_id' => 'QN2IxFFiHCTm0oKSgI6KafqJJQ3nAxwQD8QiqrYZ',
         'rest_api_key' => 'JGo5nsMzMHzODoKX70rpKh1SLQuFcTBVGnxcQp3v',
         'master_key' => 'tu7L2mISoZxJkaerbn468OZ7ZZKc5HwpqtbxuHMI',
         'javascript_key' => 'R80kznvL5F5VEzAHQ0lRwsh9iEJ3EIDdZqv1AYmJ'
-    ],
-    'amazon_bucket_url' => "https://s3-ap-southeast-1.amazonaws.com/aj-shopoye/products+/"
-    // 'parse_sdk' => [
-    //     'app_id' => '837yxeNhLEJUXZ0ys2pxnxpmyjdrBnn7BcD0vMn7',
-    //     'rest_api_key' => 'zdoU2CuhK5S1Dbi2WDb6Rcs4EgprFrrpiWx3fUBy',
-    //     'master_key' => 'aoyAfAqjov2pk7sDlKpMss8pE0EFFyjJyRKfUEJC',
-    //     'javascript_key' => 'bSqZFU0TvlhVb92R3V2fvdh4eL8pnka7kvOp11Rq'
-    // ]
+    ],*/
+    'amazon_bucket_url' => "https://s3-ap-southeast-1.amazonaws.com/aj-shopoye/products+/",
+     'parse_sdk' => [
+         'app_id' => '837yxeNhLEJUXZ0ys2pxnxpmyjdrBnn7BcD0vMn7',
+         'rest_api_key' => 'zdoU2CuhK5S1Dbi2WDb6Rcs4EgprFrrpiWx3fUBy',
+         'master_key' => 'aoyAfAqjov2pk7sDlKpMss8pE0EFFyjJyRKfUEJC',
+         'javascript_key' => 'bSqZFU0TvlhVb92R3V2fvdh4eL8pnka7kvOp11Rq'
+     ],
+    "page_limit" => 10,
 ];
+ 
