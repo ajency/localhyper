600c955e611cfb7b828ef44afa279abc7e570cec
Nutan Kamat <nutan@ajency.in>
2015-08-10 16:32:34 +0530

    no message

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@ return [
     //     'master_key' => 'tu7L2mISoZxJkaerbn468OZ7ZZKc5HwpqtbxuHMI',
     //     'javascript_key' => 'R80kznvL5F5VEzAHQ0lRwsh9iEJ3EIDdZqv1AYmJ'
     // ],
-    'amazon_bucket_url' => "https://s3-ap-southeast-1.amazonaws.com/aj-shopoye/products+/"
+    'amazon_bucket_url' => "https://s3-ap-southeast-1.amazonaws.com/aj-shopoye/products+/",
     'parse_sdk' => [
         'app_id' => '837yxeNhLEJUXZ0ys2pxnxpmyjdrBnn7BcD0vMn7',
         'rest_api_key' => 'zdoU2CuhK5S1Dbi2WDb6Rcs4EgprFrrpiWx3fUBy',
