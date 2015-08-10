b63a04227dd60ec0eccaec63118b12fb4ce4a250
Deepak Prabhudesai <deepak@ajency.in>
2015-08-10 11:58:39 +0530

    Fixed: #97

--- Seller/www/app/auth/verify-begin.js
+++ Seller/www/app/auth/verify-begin.js
@@ -1,5 +1,5 @@
 angular.module('LocalHyper.auth').controller('VerifyBeginCtrl', [
-  '$scope', 'App', 'CToast', 'User', 'UIMsg', function($scope, App, CToast, User, UIMsg) {
+  '$scope', 'App', 'CToast', 'User', 'UIMsg', 'Storage', function($scope, App, CToast, User, UIMsg, Storage) {
     $scope.user = {
       name: '',
       phone: '',
@@ -19,11 +19,19 @@ angular.module('LocalHyper.auth').controller('VerifyBeginCtrl', [
         }
       },
       nextStep: function() {
-        var state;
         if (App.isOnline()) {
-          User.info('set', $scope.user);
-          state = App.isAndroid() ? 'verify-auto' : 'verify-manual';
-          return App.navigate(state);
+          return Storage.bussinessDetails('get').then((function(_this) {
+            return function(details) {
+              details['phone'] = _this.phone;
+              details['name'] = _this.name;
+              return Storage.bussinessDetails('set', details).then(function() {
+                var state;
+                User.info('set', $scope.user);
+                state = App.isAndroid() ? 'verify-auto' : 'verify-manual';
+                return App.navigate(state);
+              });
+            };
+          })(this));
         } else {
           return CToast.show(UIMsg.noInternet);
         }
