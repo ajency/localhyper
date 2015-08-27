946e24d627a4db34d11ab922774743a3942f7bdf
Ajency <alisha@ajency.in>
2015-08-27 12:29:11 +0530

    sellers list html change

--- resources/views/admin/sellerlist.blade.php
+++ resources/views/admin/sellerlist.blade.php
@@ -29,9 +29,9 @@
               <tr >
                 <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['name'] }}</td>
                 <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['area'] }}</td>
-                <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['brands'] }}</td><a type="button" class="btn btn-link" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+                <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['brands'] }}<a type="button" class="btn btn-link" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
   View More
-</a>  
+</a>  </td>
                 <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['categories'] }}</td>
                 <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['offersCount'] }}</td>
                 <td onclick="location.href='{{ url('admin/seller/'.$seller['id']) }}'">{{ $seller['offerAcceptedCount'] }}</td>
