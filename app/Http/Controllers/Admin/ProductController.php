f5f1f1c9b383b51aa5d6223767d80a020a08a4c5
Prajay <prajay@ajency.in>
2015-09-03 15:27:59 +0530

    added color to placeholder

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -503,7 +503,7 @@ class ProductController extends Controller
 									if ((strpos($data['Image'],'amazonaws') !== false)||(is_null($data['Image']))) {
 										$imageUrl = $data['Image']; //same as what is passed in the sheet
 										if($imageUrl=='')
-											$imageUrl = "https://placeholdit.imgix.net/~text?txtsize=85&txt=".$namedData["Brand"]."&w=350&h=150&txttrack=1";
+											$imageUrl = "https://placeholdit.imgix.net/~text?txtsize=85&txt=".$namedData["Brand"]."&w=350&h=150&txttrack=1&txtclr=000000";
 
 									}
 									else{
